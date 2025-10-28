a1855ddd89a91c685e046cefce327aaa79139b46
Sushant Rasaili <sushant.mcity@gmail.com>
2025-10-28 10:33:19 +0545

    chore: home page to login page navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { ProtectedRoute } from "./guards/ProtectedRoute";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { Login } from "./pages/login/login";
 import { Products } from "./pages/products/products";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<div> Please visit login page</div>} />
+      <Route
+        path="/"
+        element={
+          <div>
+            <Link to="/login"> Login</Link>
+            Please visit login page
+          </div>
+        }
+      />
       <Route path="/login" element={<Login />} />
       {/* <Route path="/terms" element={<Home />} /> */}
       <Route
